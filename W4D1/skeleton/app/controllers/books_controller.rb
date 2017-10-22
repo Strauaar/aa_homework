4a351e72c589ada5ec6fa970b69c31a11c1934f7
Aaron <astrauli@gmail.com>
2017-10-21 22:56:50 -0700

    complete W4D1

--- W4D1/skeleton/app/controllers/books_controller.rb
+++ W4D1/skeleton/app/controllers/books_controller.rb
@@ -1,6 +1,7 @@
 class BooksController < ApplicationController
   def index
     # your code here
+    @books = Book.all
   end
 
   def new
@@ -9,10 +10,18 @@ class BooksController < ApplicationController
 
   def create
     # your code here
+    Book.create(book_params)
+    redirect_to action: 'index'
   end
 
   def destroy
     # your code here
+    book = Book.find_by(id: params[:id])
+    if book.delete
+      redirect_to action: 'index'
+    else
+      render text: book.errors.full_messages
+    end
   end
 
   private
