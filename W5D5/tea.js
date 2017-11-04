a9ffb4d5b9e62c8d50785961d994bdd4617a03a2
Aaron <astrauli@gmail.com>
2017-11-03 21:18:30 -0700

    finish webpack

--- W5D5/tea.js
+++ W5D5/tea.js
@@ -2,25 +2,30 @@ const readline = require('readline');
 
 const reader = readline.createInterface({
   input: process.stdin,
-  output: process.stdout
+  output: process.stdout,
+  terminal: false,
+
 });
 
-function teaAndBiscuits () {
-  let first, second;
-
-  reader.question('Would you like some tea?', (res) => {
-    first = res;
-    const firstRes = (first === 'yes') ? 'do' : 'don\'t';
+let first, second;
+//
+function secondQuestion() {
+  reader.question('Would you like some biscuits?', (res) => {
+    second = res;
+    const secondRes = (second === 'yes') ? 'do' : 'don\'t';
     console.log(`You replied ${res}.`);
-
-    reader.question('Would you like some biscuits?', (res) => {
-      second = res;
-      const secondRes = (second === 'yes') ? 'do' : 'don\'t';
-      console.log(`You replied ${res}.`);
-      console.log(`So you ${firstRes} want tea and you ${secondRes} want biscuits.`);
-      reader.close();
-    });
+    console.log(`So you ${firstRes} want tea and you ${secondRes} want biscuits.`);
+    reader.close();
   });
-}
+};
 
-teaAndBiscuits();
+function teaAndBiscuits () {
+  reader.question('Would you like some tea?', function (res){
+    first = res;
+    firstRes = (first === 'yes') ? 'do' : 'don\'t';
+    console.log(`You replied ${res}.`);
+    secondQuestion();
+  });
+};
+
+// teaAndBiscuits();
