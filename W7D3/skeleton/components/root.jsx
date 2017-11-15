7d0f747bbede7e150f6b8f8d33b1a1ce55dd080f
Aaron <astrauli@gmail.com>
2017-11-14 21:51:17 -0800

    finish redux cycle

--- W7D3/skeleton/components/root.jsx
+++ W7D3/skeleton/components/root.jsx
@@ -2,3 +2,19 @@ import React from 'react';
 import { Provider } from 'react-redux';
 
 import GiphysSearchContainer from './giphys_search_container';
+
+class Root extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return(
+      <Provider store={this.props.thastore}>
+        <GiphysSearchContainer />
+      </Provider>
+    )
+  }
+}
+
+export default Root;
